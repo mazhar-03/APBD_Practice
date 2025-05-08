133a0c3d8035fb3e0ea01339836a8644eab4324a
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-05-08 10:41:29 +0200

    add scripts to db

---
+++ scripts/create.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE Country (
+                         Id INT IDENTITY(1,1) PRIMARY KEY,
+                         Name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Currency (
+                          Id INT IDENTITY(1,1) PRIMARY KEY,
+                          Name VARCHAR(100) NOT NULL UNIQUE,
+                          Rate FLOAT(3) NOT NULL
+);
+
+CREATE TABLE Currency_Country (
+                                  Country_Id INT,
+                                  Currency_Id INT,
+                                  PRIMARY KEY (Country_Id, Currency_Id),
+                                  FOREIGN KEY (Country_Id) REFERENCES Country(Id),
+                                  FOREIGN KEY (Currency_Id) REFERENCES Currency(Id)
+);
---
+++ scripts/insert.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO Country (Name)
+VALUES
+    ('United States'),
+    ('Canada'),
+    ('Germany'),
+    ('France');
+
+INSERT INTO Currency (Name, Rate)
+VALUES
+    ('USD', 1.000),
+    ('CAD', 0.740),
+    ('EUR', 1.090),
+    ('GBP', 1.250);
+
+INSERT INTO Currency_Country (Country_Id, Currency_Id)
+VALUES
+    ((SELECT Id FROM Country WHERE Name = 'United States'), (SELECT Id FROM Currency WHERE Name = 'USD')),
+    ((SELECT Id FROM Country WHERE Name = 'Canada'), (SELECT Id FROM Currency WHERE Name = 'CAD')),
+    ((SELECT Id FROM Country WHERE Name = 'Germany'), (SELECT Id FROM Currency WHERE Name = 'EUR')),
+    ((SELECT Id FROM Country WHERE Name = 'France'), (SELECT Id FROM Currency WHERE Name = 'EUR'));
